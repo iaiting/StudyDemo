951ec93e0956cd43b99955e0f31e6840b3cfbc94
iaiting <iaiting@aliyun.com>
2020-12-21 01:01:54 +0800

    (*) 优化工程

--- PyQt5快速开发与实战/chap04/chap0402.py
+++ PyQt5快速开发与实战/chap04/chap0402.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+################################################################################
+#
+# 案例 4-2 主窗口居中显示
+#
+################################################################################
+from PyQt5.QtWidgets import QDesktopWidget, QApplication, QMainWindow
+import sys
 
 
-def main():
-    print("*******************t101:")
-    print("*******************t101:")
-    print("*******************t101:")
-    print("*******************t101:")
-    print("*******************t101:")
-    print("*******************t101:")
+################################################################################
+class Winform(QMainWindow):
+    def __init__(self, parent=None):
+        super(Winform, self).__init__(parent)
 
+
+################################################################################
 if __name__ == "__main__":
-    a = 10
-    printf()
-    main()
+    app = QApplication(sys.argv)
+    win = Winform()
+    win.show()
+    sys.exit(app.exec_())
