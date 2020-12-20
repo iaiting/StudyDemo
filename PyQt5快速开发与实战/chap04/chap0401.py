6adbce4ca8a05256155e75dcbc30d5af443c67b0
iaiting <iaiting@aliyun.com>
2020-12-21 00:51:19 +0800

    (*) 优化工程

--- PyQt5快速开发与实战/chap04/chap0401.py
+++ PyQt5快速开发与实战/chap04/chap0401.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
+
+################################################################################
+#
+# 案例 4-1 创建主窗口
+#
+################################################################################
 import sys
 from PyQt5.QtWidgets import QMainWindow, QApplication
 from PyQt5.QtGui import QIcon
@@ -10,11 +16,16 @@ from PyQt5.QtGui import QIcon
 class MainWidow(QMainWindow):
     def __init__(self, parent=None):
         super(MainWidow, self).__init__(parent)
+        self.resize(400, 200)
+        self.status = self.statusBar()
+        self.status.showMessage("这是状态栏提示", 5000)
+        self.setWindowTitle("PyQt MainWidows例子")
 
 
 ################################################################################
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon("./images/cartoon1.ico"))
     form = MainWidow()
     form.show()
     sys.exit(app.exec_())
