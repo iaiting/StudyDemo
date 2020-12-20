291cbfaf9b461c698614ff60ae69b572a92d0e6c
iaiting <iaiting@aliyun.com>
2020-12-21 00:27:02 +0800

    (*) 优化工程

--- PyQt5快速开发与实战/chap04/chap0401.py
+++ PyQt5快速开发与实战/chap04/chap0401.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+import sys
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtGui import QIcon
 
 
-def main():
-    print("*******************t101:")
-    print("*******************t101:")
-    print("*******************t101:")
-    print("*******************t101:")
-    print("*******************t101:")
-    print("*******************t101:")
+################################################################################
+class MainWidow(QMainWindow):
+    def __init__(self, parent=None):
+        super(MainWidow, self).__init__(parent)
 
-if __name__ == "__main__":
-    main()
+
+################################################################################
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    form = MainWidow()
+    form.show()
+    sys.exit(app.exec_())
