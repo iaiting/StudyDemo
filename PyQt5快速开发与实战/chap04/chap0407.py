000db55ae0698eb316459c783a7fcd8001e2040a
iaiting <iaiting@aliyun.com>
2020-12-24 01:07:17 +0800

    (*) 优化工程

--- PyQt5快速开发与实战/chap04/chap0407.py
+++ PyQt5快速开发与实战/chap04/chap0407.py
@@ -3,6 +3,8 @@
 
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QMainWindow
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
 
 
 class WindowDemo(QWidget):
@@ -15,9 +17,15 @@ class WindowDemo(QWidget):
 
         # 1 初始化标签控件
         label1.setText("这是一个文本标签。1")
-        label2.setText("这是一个文本标签2。2")
-        label3.setText("这是一个文本标签3。3")
-        label4.setText("4这是一个文本标签4。4")
+
+        label2.setText("<a href='#'>欢迎使用Python GUI 应用</a>")
+
+        label3.setAlignment(Qt.AlignCenter)
+        label3.setToolTip('这是一个图片标签')
+        label3.setPixmap(QPixmap("./images/python.jpg"))
+
+        label4.setText("<a href='htttp://www.baidu.com'>欢迎访问百度</a>")
+        label4.setToolTip('这是一个超级连接标签')
 
         # 2 在窗口布局中添加控件
         vbox = QVBoxLayout()
