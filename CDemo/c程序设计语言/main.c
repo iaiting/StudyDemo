ca1c43ef7eda6c16dee15915bf508b33962f984f
iaiting <iaiting@aliyun.com>
2021-01-14 20:08:01 +0800

    (*) 优化工程

---
+++ CDemo/c程序设计语言/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char *argv) {
+	printf("***********t101\n");
+	printf("***********t101\n");
+	printf("***********t101\n");
+	printf("***********t101\n");
+	printf("***********t101\n");
+	printf("***********t101\n");
+	printf("***********t101\n");
+	printf("***********t101\n");
+	printf("***********t101\n");
+	printf("***********t101\n");
+}
