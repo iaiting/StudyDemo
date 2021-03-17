1296eb5031afd5840b4bfea7ae417835b0681445
iaiting <iaiting@aliyun.com>
2021-03-17 15:34:07 +0800

    (*) 优化工程

---
+++ PyDemo/快速掌握PyQt5/chap03_布局管理/chap03_001.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+################################################################################
+#
+# 3.1 垂直布局QVBoxLayout
+#
+################################################################################
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
+
+
+################################################################################
+class Demo(QWidget):
+    def __init__(self):
+        super(Demo, self).__init__()
+        self.user_label = QLabel('Username', self)
+        self.pwd_label = QLabel('Password', self)
+
+        self.v_layout = QVBoxLayout()
+        self.v_layout.addWidget(self.user_label)
+        self.v_layout.addWidget(self.pwd_label)
+        self.setLayout(self.v_layout)
+
+
+################################################################################
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    demo = Demo()
+    demo.show()
+    sys.exit(app.exec_())
