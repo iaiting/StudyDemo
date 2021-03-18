7e329ad6239d9c74560d0ee0fb2a2c2b11337d0e
iaiting <iaiting@aliyun.com>
2021-03-18 16:45:27 +0800

    (*) 优化工程

---
+++ PyDemo/快速掌握PyQt5/chap04_QMessageBox消息框/chap04_001.py
@@ -0,0 +1,34 @@
+#!/usr/bin/evn python3
+# -*- coding: utf-8 -*-
+
+################################################################################
+#
+# 4.1 各种类型的消息框
+#
+################################################################################
+import sys
+from PyQt5.QtWidgets import (
+    QApplication, QWidget,
+    QPushButton, QMessageBox
+)
+
+
+################################################################################
+class Demo(QWidget):
+    def __init__(self):
+        super(Demo, self).__init__()
+        self.button = QPushButton('information', self)
+        self.button.clicked.connect(self.show_messagebox)
+
+    def show_messagebox(self):
+        QMessageBox.information(self, 'Title', 'Content', QMessageBox.Yes|QMessageBox.No|QMessageBox.Cancel)
+
+
+################################################################################
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    demo = Demo()
+    demo.show()
+    sys.exit(app.exec_())
+
+
