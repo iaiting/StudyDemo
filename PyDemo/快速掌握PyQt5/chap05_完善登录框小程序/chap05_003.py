f3c97da7668b3dfe8bf373d1fa008dac362680c2
iaiting <iaiting@aliyun.com>
2021-03-19 14:39:22 +0800

    (*) 优化工程

--- PyDemo/快速掌握PyQt5/chap05_完善登录框小程序/chap05_003.py
+++ PyDemo/快速掌握PyQt5/chap05_完善登录框小程序/chap05_003.py
@@ -3,14 +3,74 @@
 
 ################################################################################
 #
-# 5.3 完善注册界面布局及功能
+# 5.3 完善注册界面布局及功能 与 5.4 整合登录界面和注册界面
 #
 ################################################################################
 import sys
 from PyQt5.QtWidgets import (
     QApplication, QWidget, QDialog, QMessageBox,
     QLabel, QLineEdit, QHBoxLayout, QVBoxLayout, QPushButton,
+    QGridLayout,
 )
+
+USER_PWD = {
+    'abc': '123'
+}
+
+################################################################################
+class Demo(QWidget):
+    def __init__(self):
+        super(Demo, self).__init__()
+        self.resize(300, 100)
+
+        self.user_label = QLabel('Username: ', self)
+        self.user_line = QLineEdit(self)
+
+        self.pwd_label = QLabel('Password: ', self)
+        self.pwd_line = QLineEdit(self)
+
+        self.login_button = QPushButton('Log in', self)
+        self.signin_button = QPushButton('Sign in', self)
+
+        self.grid_layout = QGridLayout()
+        self.h_layout = QHBoxLayout()
+        self.v_layout = QVBoxLayout()
+
+        self.layout_init()
+        self.pushbutton_init()
+        self.signin_page = SigninPage()
+
+
+    def layout_init(self):
+        self.grid_layout.addWidget(self.user_label, 0, 0, 1, 1)
+        self.grid_layout.addWidget(self.user_line, 0, 1, 1, 1)
+
+        self.grid_layout.addWidget(self.pwd_label, 1, 0, 1, 1)
+        self.grid_layout.addWidget(self.pwd_line, 1, 1, 1, 1)
+
+        self.h_layout.addWidget(self.login_button)
+        self.h_layout.addWidget(self.signin_button)
+
+        self.v_layout.addLayout(self.grid_layout)
+        self.v_layout.addLayout(self.h_layout)
+
+        self.setLayout(self.v_layout)
+
+    def pushbutton_init(self):
+        self.login_button.setEnabled(False)
+        self.login_button.clicked.connect(self.check_login_func)
+        self.signin_button.clicked.connect(self.show_signin_page_func)
+
+    def check_login_func(self):
+        if USER_PWD.get(self.user_line.text()) == self.pwd_line.text():
+            QMessageBox.information(self, 'Information', 'Log in Successfully!')
+        else:
+            QMessageBox.critical(self, 'Wrong', 'Wrong Username or Password!')
+        self.pwd_line.clear()
+        self.user_line.clear()
+
+    def show_signin_page_func(self):
+        self.signin_page.exec_()
 
 
 ################################################################################
@@ -78,7 +138,7 @@ class SigninPage(QDialog):
 ################################################################################
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    signin = SigninPage()
+    signin = Demo()
     signin.show()
     sys.exit(app.exec_())
 
