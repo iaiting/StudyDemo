1c2bdfb27872149483ffb7c7df374b907973653d
iaiting <iaiting@aliyun.com>
2021-03-19 17:17:15 +0800

    (*) 优化工程

---
+++ PyDemo/快速掌握PyQt5/chap07_各种按钮介绍/chap07_003.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+################################################################################
+#
+# 7.3 QRadioButton
+#
+################################################################################
+import sys
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import (
+    QApplication, QWidget,
+    QRadioButton, QLabel,
+    QHBoxLayout, QVBoxLayout
+)
+
+
+################################################################################
+class Demo(QWidget):
+    def __init__(self):
+        super(Demo, self).__init__()
+        self.off_button = QRadioButton('off', self)
+        self.on_button = QRadioButton('on', self)
+
+        self.pic_label = QLabel(self)
+
+        self.pic_h_layout = QHBoxLayout()
+        self.button_h_layout = QHBoxLayout()
+        self.all_v_layout = QVBoxLayout()
+
+        self.layout_init()
+        self.radiobutton_init()
+        self.label_init()
+
+    def layout_init(self):
+        self.setLayout(self.all_v_layout)
+        self.all_v_layout.addLayout(self.pic_h_layout)
+        self.all_v_layout.addLayout(self.button_h_layout)
+
+        self.pic_h_layout.addStretch(1)
+        self.pic_h_layout.addWidget(self.pic_label)
+        self.pic_h_layout.addStretch(1)
+
+        self.button_h_layout.addWidget(self.off_button)
+        self.button_h_layout.addWidget(self.on_button)
+
+    def label_init(self):
+        self.pic_label.setPixmap(QPixmap('images/close.ico'))
+
+    def radiobutton_init(self):
+        self.off_button.setChecked(True)
+        self.off_button.toggled.connect(self.on_off_bulb_func)
+        pass
+
+    def on_off_bulb_func(self):
+        if self.off_button.isChecked():
+            self.pic_label.setPixmap(QPixmap('images/close.ico'))
+        else:
+            self.pic_label.setPixmap(QPixmap('images/doc.ico'))
+
+
+################################################################################
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    demo = Demo()
+    demo.show()
+    sys.exit(app.exec_())
+
