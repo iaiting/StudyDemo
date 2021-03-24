fef4ed34860ad8a50fc693012ed79b34c2188cc9
iaiting <iaiting@aliyun.com>
2021-03-24 17:31:10 +0800

    (*) 优化工程

--- PyDemo/快速掌握PyQt5/chap12_与日期时间相关的控件/chap12_002.py
+++ PyDemo/快速掌握PyQt5/chap12_与日期时间相关的控件/chap12_002.py
@@ -9,7 +9,7 @@
 import sys
 from PyQt5.QtCore import QDate, QTime, QDateTime
 from PyQt5.QtWidgets import (
-    QApplication, QWidget, QDateTimeEdit,
+    QApplication, QWidget, QDateTimeEdit, QDateEdit, QTimeEdit,
     QVBoxLayout,
 )
 
@@ -19,14 +19,25 @@ class Demo(QWidget):
     def __init__(self):
         super(Demo, self).__init__()
         self.datetime_1 = QDateTimeEdit(self)
+        self.datetime_1.dateChanged.connect(lambda : print('Date Changed!'))
 
         self.datetime_2 = QDateTimeEdit(QDateTime.currentDateTime(), self)
+        self.datetime_2.setDisplayFormat('yyyy-MM-dd HH:mm:ss')
+        self.datetime_2.dateChanged.connect(lambda: print('Time Changed!'))
 
         self.datetime_3 = QDateTimeEdit(QDateTime.currentDateTime(), self)
+        self.datetime_3.dateChanged.connect(lambda: print('DateTime Changed!'))
+        self.datetime_3.setCalendarPopup(True)
 
         self.datetime_4 = QDateTimeEdit(QDate.currentDate(), self)
 
         self.datetime_5 = QDateTimeEdit(QTime.currentTime(), self)
+
+        self.date = QDateEdit(QDate.currentDate(), self)
+        self.date.setDisplayFormat('yyyy/MM/dd')
+
+        self.time = QTimeEdit(QTime.currentTime(), self)
+        self.time.setDisplayFormat('HH:mm:ss')
 
         self.v_layout = QVBoxLayout()
         self.v_layout.addWidget(self.datetime_1)
@@ -34,6 +45,9 @@ class Demo(QWidget):
         self.v_layout.addWidget(self.datetime_3)
         self.v_layout.addWidget(self.datetime_4)
         self.v_layout.addWidget(self.datetime_5)
+
+        self.v_layout.addWidget(self.date)
+        self.v_layout.addWidget(self.time)
 
         self.setLayout(self.v_layout)
 
