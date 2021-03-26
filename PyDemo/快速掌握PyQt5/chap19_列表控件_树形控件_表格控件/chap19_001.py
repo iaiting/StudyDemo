a330520f73b381fd1b8e0290e2f597ee8e910314
iaiting <iaiting@aliyun.com>
2021-03-26 15:36:09 +0800

    (*) 优化工程

---
+++ PyDemo/快速掌握PyQt5/chap19_列表控件_树形控件_表格控件/chap19_001.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+################################################################################
+#
+# 19.1 列表控件QListWidget
+#
+################################################################################
+import sys
+from PyQt5.QtWidgets import (
+    QApplication, QWidget, QLabel, QListWidget, QListWidgetItem,
+    QHBoxLayout, QVBoxLayout
+)
+
+
+################################################################################
+class Demo(QWidget):
+    def __init__(self):
+        super(Demo, self).__init__()
+        self.pic_label = QLabel('arrow.png', self)
+
+        self.listwidget_1 = QListWidget(self)
+        self.listwidget_1.doubleClicked.connect(lambda : self.change_func(self.listwidget_1))
+        self.listwidget_2 = QListWidget(self)
+        self.listwidget_2.doubleClicked.connect(lambda: self.change_func(self.listwidget_2))
+
+
+        for i in range(6):
+            text = 'Item {}'.format(i)
+            self.item = QListWidgetItem(text)
+            self.listwidget_1.addItem(self.item)
+
+        self.item_6 = QListWidgetItem('Item 6', self.listwidget_1)
+        self.listwidget_1.addItem('Item 7')
+
+        str_list = ['Item 9', 'Item 10']
+        self.listwidget_1.addItems(str_list)
+
+        self.item_8 = QListWidgetItem('Item 8')
+        self.listwidget_1.insertItem(8, self.item_8)
+
+        self.h_layout = QHBoxLayout()
+        self.h_layout.addWidget(self.listwidget_1)
+        self.h_layout.addWidget(self.pic_label)
+        self.h_layout.addWidget(self.listwidget_2)
+        self.setLayout(self.h_layout)
+
+
+    def change_func(self, listwidget):
+        print("change_func")
+        if listwidget == self.listwidget_1:
+            item = QListWidgetItem(self.listwidget_1.currentItem())
+            self.listwidget_2.addItem(item)
+        else:
+            self.listwidget_2.takeItem(self.listwidget_2.currentRow())
+            pass
+
+################################################################################
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    demo = Demo()
+    demo.show()
+    sys.exit(app.exec_())
