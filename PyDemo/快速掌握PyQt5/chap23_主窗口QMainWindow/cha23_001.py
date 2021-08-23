a7288e4f2851f295b8749df5d4b3c3fba49bc28e
iaiting <iaiting@aliyun.com>
2021-08-23 17:17:50 +0800

    (*) 优化代码

---
+++ PyDemo/快速掌握PyQt5/chap23_主窗口QMainWindow/cha23_001.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+################################################################################
+#
+# 23.1 记事本
+#
+###############################################################################
+import sys
+
+
+from PyQt5.QtWidgets import QMainWindow, QApplication
+
+###############################################################################
+class Demo(QMainWindow):
+
+    def __init__(self):
+        super(Demo, self).__init__()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    demo = Demo()
+    demo.show()
+    sys.exit(app.exec_())
