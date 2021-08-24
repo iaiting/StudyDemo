3324a2139c8b58cc59f7d5305bbe8a12a0901618
iaiting <iaiting@aliyun.com>
2021-08-24 16:22:39 +0800

    (*) 优化工程

--- PyDemo/快速掌握PyQt5/chap23_主窗口QMainWindow/cha23_001.py
+++ PyDemo/快速掌握PyQt5/chap23_主窗口QMainWindow/cha23_001.py
@@ -7,17 +7,31 @@
 #
 ###############################################################################
 import sys
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtWidgets import QMainWindow, QApplication, QAction
 
 
 ###############################################################################
 class Demo(QMainWindow):
+    is_saved = True
+    is_saved_first = True
+    path = ''
 
     def __init__(self):
         super(Demo, self).__init__()
 
+        self.file_menu = self.menuBar().addMenu('File')
+        self.edit_menu = self.menuBar().addMenu('Edit')
+        self.help_menu = self.menuBar().addMenu('Help')
 
-###############################################################################
+        self.file_toolbar = self.addToolBar('File')
+        self.edit_toolbar = self.addToolBar('Edit')
+
+        self.status_bar = self.statusBar()
+
+        self.new_action = QAction('New', self)
+
+
+#############################################################################
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     demo = Demo()
