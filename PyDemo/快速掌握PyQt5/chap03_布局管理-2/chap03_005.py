dec3ac84649cc955d532f73104e21f17e8ff549d
iaiting <iaiting@aliyun.com>
2021-08-28 00:56:30 +0800

    (*) 优化工程

---
+++ PyDemo/快速掌握PyQt5/chap03_布局管理-2/chap03_005.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+################################################################################
+#
+# 3.5 网格布局QGridLayout
+#
+################################################################################
+import sys
+
+from PyQt5.QtWidgets import (
+    QApplication, QGridLayout, QHBoxLayout, QLabel, QLineEdit, QPushButton, QVBoxLayout, QWidget,
+    QHBoxLayout, QVBoxLayout, QGridLayout, 
+)
+
+
+################################################################################
+class Demo(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.user_label = QLabel('Username:', self)
+        self.user_line = QLineEdit(self)
+
+        self.pwd_label = QLabel('Password:', self)
+        self.pwd_line = QLineEdit(self)
+
+        self.login_btn = QPushButton('Log in', self)
+        self.signin_btn = QPushButton('Sign in', self)
+
+        self.v_layout = QVBoxLayout()
+        self.g_layout = QGridLayout()
+        self.h_layout = QHBoxLayout()
+        self.v_layout.addLayout(self.g_layout)
+        self.v_layout.addLayout(self.h_layout)
+        self.setLayout(self.v_layout)
+
+        self.g_layout.addWidget(self.user_label, 0, 0, 1, 1)
+        self.g_layout.addWidget(self.user_line, 0, 1, 1, 1)
+        self.g_layout.addWidget(self.pwd_label, 1, 0, 1, 1)
+        self.g_layout.addWidget(self.pwd_line, 1, 1, 1, 1)
+
+        self.h_layout.addWidget(self.login_btn)
+        self.h_layout.addWidget(self.signin_btn)
+        
+
+
+################################################################################
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    demo = Demo()
+    demo.show()
+    sys.exit(app.exec_())
