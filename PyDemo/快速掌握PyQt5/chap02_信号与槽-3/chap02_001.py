89191e3afa5870b5027bb1e080d978482dd64be5
iaiting <iaiting@aliyun.com>
2021-10-26 22:39:36 +0800

    (*) 优化工程

---
+++ PyDemo/快速掌握PyQt5/chap02_信号与槽-3/chap02_001.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+################################################################################
+#
+# 2.1 通过按钮来改变文本(一个信号连接一个槽)
+#
+################################################################################
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+
+################################################################################
+class Demo(QWidget):
+    def __init__(self):
+        super(Demo, self).__init__()
+        self.button = QPushButton('Start', self)
+        self.button.clicked.connect(self.change_text)
+
+    def change_text(self):
+        print('change text')
+        self.button.setText('Stop')
+        self.button.clicked.disconnect(self.change_text)
+
+
+################################################################################
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    demo = Demo()
+    demo.show()
+    sys.exit(app.exec_())
