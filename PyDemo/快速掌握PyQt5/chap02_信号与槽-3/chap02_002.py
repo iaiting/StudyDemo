d76cdcd9c7af33554d93786cd3e879d3a482ce2b
iaiting <iaiting@aliyun.com>
2021-10-28 16:03:29 +0800

    (*) 优化工程

---
+++ PyDemo/快速掌握PyQt5/chap02_信号与槽-3/chap02_002.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+################################################################################
+#
+# 2.2 多个信号连接同一个槽
+#
+################################################################################
+import sys
+from PyQt5.QtWidgets import QApplication, QPushButton, QWidget
+
+################################################################################
+class Demo(QWidget):
+    def __init__(self):
+        super(Demo, self).__init__()
+        self.button = QPushButton('Start', self)
+        self.button.pressed.connect(self.change_text)
+        self.button.released.connect(self.change_text)
+
+    def change_text(self):
+        if self.button.text() == 'Start':
+            self.button.setText('Stop')
+        else:
+            self.button.setText('Start')
+
+################################################################################
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    demo = Demo()
+    demo.show()
+    sys.exit(app.exec_())
