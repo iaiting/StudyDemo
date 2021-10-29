bc5898f6a57b2aecbd1b49665bf23ccf3a430391
iaiting <iaiting@aliyun.com>
2021-10-29 17:59:07 +0800

    (*) 优化工程

---
+++ PyDemo/快速掌握PyQt5/chap03_布局管理-3/chap03_003_1.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+################################################################################
+#
+# 3.3 混合使用QVBoxLayout和QHBoxLayout
+#
+################################################################################
+import sys
+from PyQt5.QtWidgets import QHBoxLayout, QVBoxLayout, QWidget, QApplication, QLabel, QLineEdit, QPushButton
+
+################################################################################
+class Demo(QWidget):
+    def __init__(self):
+        super(Demo, self).__init__()
+        self.user_label = QLabel('Username', self)
+        self.pwd_label = QLabel('Password:', self)
+        self.user_line = QLineEdit(self)
+        self.pwd_line = QLineEdit(self)
+        self.login_button = QPushButton('Log in', self)
+        self.signin_button = QPushButton('Sign in', self)
+
+        self.label_v_layout = QVBoxLayout()
+        self.line_v_layout = QVBoxLayout()
+        self.label_line_h_layout = QHBoxLayout()
+        self.button_h_layout = QHBoxLayout()
+        self.all_v_layout = QVBoxLayout()
+
+        self.label_v_layout.addWidget(self.user_label)
+        self.label_v_layout.addWidget(self.pwd_label)
+
+        self.line_v_layout.addWidget(self.user_line)
+        self.line_v_layout.addWidget(self.pwd_line)
+
+        self.button_h_layout.addWidget(self.login_button)
+        self.button_h_layout.addWidget(self.signin_button)
+
+        self.label_line_h_layout.addLayout(self.label_v_layout)
+        self.label_line_h_layout.addLayout(self.line_v_layout)
+
+        self.all_v_layout.addLayout(self.label_line_h_layout)
+        self.all_v_layout.addLayout(self.button_h_layout)
+        self.setLayout(self.all_v_layout)
+
+
+################################################################################
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    demo = Demo()
+    demo.show()
+    sys.exit(app.exec_())
