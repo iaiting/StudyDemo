ab84030f6437629efd88b45ff9ca9881ff5a5e49
iaiting <iaiting@aliyun.com>
2021-11-02 11:08:07 +0800

    (*) 优化工程

--- PyDemo/快速掌握PyQt5/chap05_完善登录框小程序-3/chap05_001.py
+++ PyDemo/快速掌握PyQt5/chap05_完善登录框小程序-3/chap05_001.py
@@ -22,21 +22,28 @@ class Demo(QWidget):
         self.user_line = QLineEdit(self)
         self.pwd_label = QLabel('Password:', self)
         self.pwd_line = QLineEdit(self)
+        
+        self.login_button = QPushButton('Log in', self)
+        self.signin_button = QPushButton('Sign in', self)
 
         self.grid_layout = QGridLayout()
         self.h_layout = QHBoxLayout()
-        self.v_layout = QVBoxLayout()
+        self.all_v_layout = QVBoxLayout()
 
         self.layout_init()
 
     def layout_init(self):
         self.grid_layout.addWidget(self.user_label, 0, 0)
         self.grid_layout.addWidget(self.user_line, 0, 1)
-
         self.grid_layout.addWidget(self.pwd_label, 1, 0)
         self.grid_layout.addWidget(self.pwd_line, 1, 1)
 
-        self.setLayout(self.grid_layout)
+        self.h_layout.addWidget(self.login_button)
+        self.h_layout.addWidget(self.signin_button)
+
+        self.all_v_layout.addLayout(self.grid_layout)
+        self.all_v_layout.addLayout(self.h_layout)
+        self.setLayout(self.all_v_layout)
 
 ################################################################################
 if __name__ == "__main__":
