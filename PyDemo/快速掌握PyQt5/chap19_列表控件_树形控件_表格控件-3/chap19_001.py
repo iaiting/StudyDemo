6575c27d995edd7d7c9592ed14a54812b78897b6
iaiting <iaiting@aliyun.com>
2021-11-03 11:03:44 +0800

    (*) 优化工程

--- PyDemo/快速掌握PyQt5/chap19_列表控件_树形控件_表格控件-3/chap19_001.py
+++ PyDemo/快速掌握PyQt5/chap19_列表控件_树形控件_表格控件-3/chap19_001.py
@@ -22,6 +22,9 @@ class Demo(QWidget):
         self.listwidget_1 = QListWidget(self)
         self.listwidget_2 = QListWidget(self)
 
+        self.listwidget_1.doubleClicked.connect(lambda: self.change_func(self.listwidget_1))
+        self.listwidget_2.doubleClicked.connect(lambda: self.change_func(self.listwidget_2))
+
         for i in range(6):
             text = 'Item {}'.format(i)
             self.item = QListWidgetItem(text)
@@ -42,6 +45,15 @@ class Demo(QWidget):
         self.h_layout.addWidget(self.listwidget_2)
 
         self.setLayout(self.h_layout)
+
+    def change_func(self, listwidget):
+        if listwidget == self.listwidget_1:
+            item = QListWidgetItem(self.listwidget_1.currentItem())
+            self.listwidget_2.addItem(item)
+            print(self.listwidget_2.count())
+        else:
+            self.listwidget_2.takeItem(self.listwidget_2.currentRow())
+            print(self.listwidget_2.count())
 
 
 ################################################################################
