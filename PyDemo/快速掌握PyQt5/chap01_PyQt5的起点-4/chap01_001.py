d7204c01fb3846911dd4ff03e2ff882e1272646f
iaiting <iaiting@aliyun.com>
2021-12-10 10:32:08 +0800

    (*) 优化工程

--- PyDemo/快速掌握PyQt5/chap01_PyQt5的起点-4/chap01_001.py
+++ PyDemo/快速掌握PyQt5/chap01_PyQt5的起点-4/chap01_001.py
@@ -11,7 +11,6 @@ from PyQt5.QtWidgets import (
     QApplication, QLabel
 )
 
-
 ################################################################################
 if __name__ == "__main__":
     app = QApplication(sys.argv)
