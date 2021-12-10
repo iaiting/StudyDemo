f3073750675b7736d250c71c348fc817105fca2d
iaiting <iaiting@aliyun.com>
2021-12-10 09:20:25 +0800

    (*) 优化工程

---
+++ PyDemo/快速掌握PyQt5/chap01_PyQt5的起点-4/chap01_001.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+################################################################################
+#
+# 1.2 程序运行起点
+#
+################################################################################
+import sys
+from PyQt5.QtWidgets import (
+    QApplication, QLabel
+)
+
+
+################################################################################
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    label = QLabel('Hello World')
+    label.show()
+    sys.exit(app.exec_())
