3b7c5b0a22e3f5eb37b701062d070a750a8b2775
iaiting <iaiting@aliyun.com>
2021-12-14 13:11:22 +0800

    (*) 优化工程

--- PyDemo/快速掌握PyQt5/chap02_信号与槽-4/chap02_004.py
+++ PyDemo/快速掌握PyQt5/chap02_信号与槽-4/chap02_004.py
@@ -15,6 +15,9 @@ from PyQt5.QtWidgets import (
 class Demo(QWidget):
     def __init__(self):
         super().__init__()
+        self.resize(300, 300)
+        self.setWindowTitle('demo')
+
         self.button = QPushButton('Start', self)
 
         self.button.clicked.connect(self.change_text)
@@ -23,12 +26,18 @@ class Demo(QWidget):
     
     def change_text(self):
         print('change text')
+        self.button.setText('Stop')
+        self.button.clicked.disconnect(self.change_text)
 
     def change_window_size(self):
         print('change window size')
+        self.resize(500, 500)
+        self.button.clicked.disconnect(self.change_window_size)
 
     def change_window_title(self):
         print('change window title')
+        self.setWindowTitle('window title changed')
+        self.button.clicked.disconnect(self.change_window_title)
 
 ################################################################################
 if __name__ == "__main__":
