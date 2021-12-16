35cb6da3a951049526439944b6328438fe43a177
iaiting <iaiting@aliyun.com>
2021-12-16 09:49:43 +0800

    (*) 优化工程

--- PyDemo/快速掌握PyQt5/chap03_布局管理-4/chap03_004.py
+++ PyDemo/快速掌握PyQt5/chap03_布局管理-4/chap03_004.py
@@ -3,17 +3,38 @@
 
 ################################################################################
 #
-#
+# 3.4 表单布局QFormLayout
 #
 ################################################################################
 import sys
 from PyQt5.QtWidgets import (
-    QApplication, QWidget
+    QApplication, QWidget, QLabel, QLineEdit, QPushButton,
+    QHBoxLayout, QVBoxLayout, QFormLayout
 )
 
 ################################################################################
 class Demo(QWidget):
-    pass
+    def __init__(self):
+        super().__init__()
+        self.user_label = QLabel('Username:', self)
+        self.pwd_label = QLabel('Password:', self)
+        self.user_line = QLineEdit(self)
+        self.pwd_line = QLineEdit(self)
+        self.login_button = QPushButton('Log in', self)
+        self.signin_button = QPushButton('Sign in', self)
+
+        self.f_layout = QFormLayout()
+        self.f_layout.addRow(self.user_label, self.user_line)
+        self.f_layout.addRow(self.pwd_label, self.pwd_line)
+
+        self.button_h_layout = QHBoxLayout()
+        self.button_h_layout.addWidget(self.login_button)
+        self.button_h_layout.addWidget(self.signin_button)
+
+        self.all_v_layout = QVBoxLayout()
+        self.all_v_layout.addLayout(self.f_layout)
+        self.all_v_layout.addLayout(self.button_h_layout)
+        self.setLayout(self.all_v_layout)
 
 ################################################################################
 if __name__ == "__main__":
