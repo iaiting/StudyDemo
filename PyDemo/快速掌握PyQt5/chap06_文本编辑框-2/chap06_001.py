70c4210b3ac708a857f7ad2081d2728b529847c7
iaiting <iaiting@aliyun.com>
2021-12-17 22:02:20 +0800

    (*) 优化工程

--- PyDemo/快速掌握PyQt5/chap06_文本编辑框-2/chap06_001.py
+++ PyDemo/快速掌握PyQt5/chap06_文本编辑框-2/chap06_001.py
@@ -9,28 +9,41 @@
 import sys
 
 from PyQt5.QtWidgets import (
-    QApplication, QHBoxLayout, QLabel, QVBoxLayout, QWidget
+    QApplication, QHBoxLayout, QLabel, QTextBrowser, QTextEdit, QVBoxLayout, QWidget
 )
 ################################################################################
 class Demo(QWidget):
     def __init__(self):
         super().__init__()
-        self.edit_label = QLabel('QTextEdit', self)
-        self.browser_label = QLabel('QTextBrowser', self)
+        self.text_edit_label = QLabel('QTextEdit', self)
+        self.text_browser_label = QLabel('QTextBrowser', self)
 
-        self.edit_v_layout = QVBoxLayout()
-        self.browser_v_layout = QVBoxLayout()
+        self.text_edit = QTextEdit()
+        self.text_browser = QTextBrowser()
+
+        self.textedit_v_layout = QVBoxLayout()
+        self.textbrowser_v_layout = QVBoxLayout()
         self.all_h_layout = QHBoxLayout()
 
         self.layout_init()
+        self.textedit_init()
 
     def layout_init(self):
-        self.all_h_layout.addLayout(self.edit_v_layout)
-        self.all_h_layout.addLayout(self.browser_v_layout)
+        self.all_h_layout.addLayout(self.textedit_v_layout)
+        self.all_h_layout.addLayout(self.textbrowser_v_layout)
         self.setLayout(self.all_h_layout)
 
-        self.edit_v_layout.addWidget(self.edit_label)
-        self.browser_v_layout.addWidget(self.browser_label)
+        self.textedit_v_layout.addWidget(self.text_edit_label)
+        self.textedit_v_layout.addWidget(self.text_edit)
+
+        self.textbrowser_v_layout.addWidget(self.text_browser_label)
+        self.textbrowser_v_layout.addWidget(self.text_browser)
+
+    def textedit_init(self):
+        self.text_edit.textChanged.connect(self.show_text_func)
+
+    def show_text_func(self):
+        self.text_browser.setText(self.text_edit.toPlainText())
 
 ################################################################################
 if __name__ == "__main__":
