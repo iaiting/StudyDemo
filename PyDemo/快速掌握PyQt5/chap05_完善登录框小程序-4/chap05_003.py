fec378c8524e2e03f129239ea2c62bdd0374f6d5
iaiting <iaiting@aliyun.com>
2021-12-17 17:21:58 +0800

    (*) 优化工程

--- PyDemo/快速掌握PyQt5/chap05_完善登录框小程序-4/chap05_003.py
+++ PyDemo/快速掌握PyQt5/chap05_完善登录框小程序-4/chap05_003.py
@@ -3,7 +3,7 @@
 
 ################################################################################
 #
-# 5.3 完善注册界面布局及功能
+# 5.3 完善注册界面布局及功能 & 5.4 整合登录界面和注册界面
 #
 ################################################################################
 import sys
@@ -86,11 +86,70 @@ class SigninPage(QDialog):
 
 ################################################################################
 class Demo(QWidget):
-    pass
+    def __init__(self):
+        super().__init__()
+        self.resize(300, 100)
+        
+        self.user_label = QLabel("Username:", self)
+        self.user_line = QLineEdit(self)
+
+        self.pwd_label = QLabel("Password:", self)
+        self.pwd_line = QLineEdit(self)
+
+        self.login_button = QPushButton('Log in', self)
+        self.signin_button = QPushButton('Sign in', self)
+
+        self.user_h_layout = QHBoxLayout()
+        self.pwd_h_layout = QHBoxLayout()
+        self.button_h_layout = QHBoxLayout()
+        self.all_v_layout = QVBoxLayout()
+
+        self.layout_init()
+        self.lineedit_init()
+        self.pushbutton_init()
+
+        self.signin_page = SigninPage()
+
+    def layout_init(self):
+        self.setLayout(self.all_v_layout)
+        self.all_v_layout.addLayout(self.user_h_layout)
+        self.all_v_layout.addLayout(self.pwd_h_layout)
+        self.all_v_layout.addLayout(self.button_h_layout)
+
+        self.user_h_layout.addWidget(self.user_label)
+        self.user_h_layout.addWidget(self.user_line)
+
+        self.pwd_h_layout.addWidget(self.pwd_label)
+        self.pwd_h_layout.addWidget(self.pwd_line)
+
+        self.button_h_layout.addWidget(self.login_button)
+        self.button_h_layout.addWidget(self.signin_button)
+
+    def lineedit_init(self):
+        self.user_line.setPlaceholderText('Please enter your username')
+        self.pwd_line.setPlaceholderText('Please enter your password')
+        self.pwd_line.setEchoMode(QLineEdit.Password)
+
+        self.user_line.textChanged.connect(self.check_input_func)
+        self.pwd_line.textChanged.connect(self.check_input_func)
+
+    def pushbutton_init(self):
+        self.login_button.setEnabled(False)
+        self.login_button.clicked.connect(self.check_login_func)
+        self.signin_button.clicked.connect(self.show_signin_page_func)
+
+    def check_input_func(self):
+        pass
+
+    def check_login_func(self):
+        pass
+
+    def show_signin_page_func(self):
+        self.signin_page.exec_()
 
 ################################################################################
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    demo = SigninPage()
+    demo = Demo()
     demo.show()
     sys.exit(app.exec_())
