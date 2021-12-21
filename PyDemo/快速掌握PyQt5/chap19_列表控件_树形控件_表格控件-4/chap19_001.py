d6f0b4dd3be6220c3207637a614fb3958102ab57
iaiting <iaiting@aliyun.com>
2021-12-21 10:58:01 +0800

    (*) 优化工程

--- PyDemo/快速掌握PyQt5/chap19_列表控件_树形控件_表格控件-4/chap19_001.py
+++ PyDemo/快速掌握PyQt5/chap19_列表控件_树形控件_表格控件-4/chap19_001.py
@@ -6,3 +6,60 @@
 # 19.1 列表控件QListWidget
 #
 ################################################################################
+import sys
+from PyQt5.QtWidgets import (
+    QApplication,
+    QHBoxLayout,
+    QLabel,
+    QListWidget,
+    QListWidgetItem,
+    QWidget
+)
+
+################################################################################
+class Demo(QWidget):
+    pass
+    def __init__(self):
+        super().__init__()
+
+        self.pic_label = QLabel('图片')
+        self.listwidget_1 = QListWidget(self)
+        self.listwidget_2 = QListWidget(self)
+
+        self.listwidget_1.doubleClicked.connect(lambda: self.change_func(self.listwidget_1))
+        self.listwidget_2.doubleClicked.connect(lambda: self.change_func(self.listwidget_2))
+
+        for i in range(6):
+            text = 'Item {}'.format(i)
+            item = QListWidgetItem(text)
+            self.listwidget_1.addItem(item)
+
+        self.item_6 = QListWidgetItem('Item 6', self.listwidget_1)
+        self.listwidget_1.addItem('Item 7')
+
+        str_list = ['Item 9', 'Item 10']
+        self.listwidget_1.addItems(str_list)
+
+        self.item_8 = QListWidgetItem('Item 8')
+        self.listwidget_1.insertItem(8, self.item_8)
+
+        self.h_layout = QHBoxLayout()
+        self.h_layout.addWidget(self.listwidget_1)
+        self.h_layout.addWidget(self.pic_label)
+        self.h_layout.addWidget(self.listwidget_2)
+        self.setLayout(self.h_layout)
+
+    def change_func(self, listwidget):
+        if listwidget == self.listwidget_1:
+            print(self.listwidget_1.count())
+            item = QListWidgetItem(self.listwidget_1.currentItem())
+            self.listwidget_2.addItem(item)
+        else:
+            print(self.listwidget_2.count())
+            self.listwidget_2.takeItem(self.listwidget_2.currentRow())
+################################################################################
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    demo = Demo()
+    demo.show()
+    sys.exit(app.exec_())
--- PyDemo/快速掌握PyQt5/chap19_列表控件_树形控件_表格控件-4/chap19_002.py
+++ PyDemo/快速掌握PyQt5/chap19_列表控件_树形控件_表格控件-4/chap19_002.py
@@ -8,7 +8,7 @@
 ################################################################################
 import sys
 from PyQt5.QtWidgets import (
-    QApplication, QWidget,
+    QApplication, QHBoxLayout, QTreeWidget, QTreeWidgetItem, QWidget,
 )
 
 ################################################################################
@@ -16,6 +16,32 @@ class Demo(QWidget):
     pass
     def __init__(self):
         super().__init__()
+
+        self.resize(500, 300)
+
+        self.tree = QTreeWidget(self)
+        self.preview = QTreeWidgetItem(self.tree)
+
+        self.qt5112 = QTreeWidgetItem()
+        self.qt5112.setText(0, 'Qt 5.11.2 snapshot')
+        self.preview.addChild(self.qt5112)
+
+        choice_list = ['macOS', 'Android x86', 'Android ARMv7']
+        for i, c in enumerate(choice_list):
+            print(i, c)
+            item = QTreeWidgetItem(self.qt5112)
+            item.setText(0, c)
+
+        self.test_item = QTreeWidgetItem(self.qt5112)
+        self.test_item.setText(0, 'test1')
+        self.test_item.setText(1, 'test2')
+
+        self.tree.expandAll()
+
+        self.h_layout = QHBoxLayout()
+        self.h_layout.addWidget(self.tree)
+
+        self.setLayout(self.h_layout)
 
 ################################################################################
 if __name__ == "__main__":
