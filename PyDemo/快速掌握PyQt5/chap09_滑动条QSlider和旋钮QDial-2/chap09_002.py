cc3029b9a7ab875bb46489fda21cf90f4ed62ea5
iaiting <iaiting@aliyun.com>
2021-12-31 11:13:16 +0800

    (*) 优化工程

--- PyDemo/快速掌握PyQt5/chap09_滑动条QSlider和旋钮QDial-2/chap09_002.py
+++ PyDemo/快速掌握PyQt5/chap09_滑动条QSlider和旋钮QDial-2/chap09_002.py
@@ -7,8 +7,12 @@
 #
 ################################################################################
 import sys
+from PyQt5.QtGui import QFont
 from PyQt5.QtWidgets import (
     QApplication,
+    QDial,
+    QHBoxLayout,
+    QLabel,
     QWidget
 )
 
@@ -17,6 +21,25 @@ class Demo(QWidget):
     def __init__(self) -> None:
         super().__init__()
         self.setWindowTitle('QDial')
+        
+        self.dial = QDial()
+        self.dial.setFixedSize(100, 100)
+        self.dial.setRange(10, 50)
+        self.dial.setNotchesVisible(True)
+        self.dial.valueChanged.connect(self.on_change_func)
+
+        self.label = QLabel('0')
+        self.label.setFont(QFont('Arial Black', 20))
+        # self.label.setFixedWidth(20)
+
+        self.h_layout = QHBoxLayout()
+        self.setLayout(self.h_layout)
+    
+        self.h_layout.addWidget(self.dial)
+        self.h_layout.addWidget(self.label)
+
+    def on_change_func(self):
+        self.label.setText(str(self.dial.value()))
 
 ################################################################################
 if __name__ == "__main__":
