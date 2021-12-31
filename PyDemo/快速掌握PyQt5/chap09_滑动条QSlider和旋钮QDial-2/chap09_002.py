c138205dc7a2d5dab92c4bfa99fee809c66a4ca2
iaiting <iaiting@aliyun.com>
2021-12-31 11:01:17 +0800

    (*) 优化工程

--- PyDemo/快速掌握PyQt5/chap09_滑动条QSlider和旋钮QDial-2/chap09_002.py
+++ PyDemo/快速掌握PyQt5/chap09_滑动条QSlider和旋钮QDial-2/chap09_002.py
@@ -16,6 +16,7 @@ from PyQt5.QtWidgets import (
 class Demo(QWidget):
     def __init__(self) -> None:
         super().__init__()
+        self.setWindowTitle('QDial')
 
 ################################################################################
 if __name__ == "__main__":
