74496293ba09959727da04113a9d345d797df577
iaiting <iaiting@aliyun.com>
2022-01-06 15:46:10 +0800

    (*) 优化工程

--- PyDemo/快速掌握PyQt5/chap22_各种对话框介绍-3/chap22_002.py
+++ PyDemo/快速掌握PyQt5/chap22_各种对话框介绍-3/chap22_002.py
@@ -8,7 +8,7 @@
 ################################################################################
 import sys
 from PyQt5.QtWidgets import (
-    QApplication, QGridLayout, QLineEdit, QPushButton, QWidget
+    QApplication, QGridLayout, QInputDialog, QLineEdit, QPushButton, QTextEdit, QWidget
 )
 
 ################################################################################
@@ -28,10 +28,14 @@ class Demo(QWidget):
         self.score_btn = QPushButton('Score', self)
         self.score_line = QLineEdit(self)
 
+        self.info_btn = QPushButton('Info', self)
+        self.info_textedit = QTextEdit(self)
+
         self.name_btn.clicked.connect(lambda: self.open_dialog_func(self.name_btn))
         self.gender_btn.clicked.connect(lambda: self.open_dialog_func(self.gender_btn))
         self.age_btn.clicked.connect(lambda: self.open_dialog_func(self.age_btn))
         self.score_btn.clicked.connect(lambda: self.open_dialog_func(self.score_btn))
+        self.info_btn.clicked.connect(lambda: self.open_dialog_func(self.info_btn))
 
         self.grid_layout = QGridLayout()
         self.setLayout(self.grid_layout)
@@ -48,18 +52,39 @@ class Demo(QWidget):
         self.grid_layout.addWidget(self.score_btn, 3, 0, 1, 1)
         self.grid_layout.addWidget(self.score_line, 3, 1, 1, 1)
 
+        self.grid_layout.addWidget(self.info_btn, 4, 0, 1, 1)
+        self.grid_layout.addWidget(self.info_textedit, 4, 1, 1, 1)
+
 
     def open_dialog_func(self, btn):
         if btn == self.name_btn:
             print('name_btn')
+            name, ok = QInputDialog.getText(self, 'Name Input', 'Please enter the name:')
+            if ok:
+                self.name_line.setText(name)
+
         elif btn == self.gender_btn:
             print('gender_btn')
+            gender_list = ['Female', 'Male']
+            gender, ok = QInputDialog.getItem(self, 'Gender Input', 'Please choose the gender:', gender_list)
+            if ok:
+                self.gender_line.setText(gender)
+
         elif btn == self.age_btn:
             print('age_btn')
+            age, ok = QInputDialog.getInt(self, 'Age Input', 'Please select the age:')
+            if ok:
+                self.age_line.setText(str(age))
         elif btn == self.score_btn:
             print('score_btn')
+            score, ok = QInputDialog.getDouble(self, 'Score Input', 'Please select the score:')
+            if ok:
+                self.score_line.setText(str(score))
         else:
             print('else')
+            info, ok = QInputDialog.getMultiLineText(self, 'Info Input', 'Please enter the info:')
+            if ok:
+                self.info_textedit.setText(info)
 
 ################################################################################
 if __name__ == "__main__":
