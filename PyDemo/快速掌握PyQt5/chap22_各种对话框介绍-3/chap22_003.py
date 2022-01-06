727a70e8e17b6d039ce6c12d7f04d92be60f46f4
iaiting <iaiting@aliyun.com>
2022-01-06 16:05:51 +0800

    (*) 优化工程

--- PyDemo/快速掌握PyQt5/chap22_各种对话框介绍-3/chap22_003.py
+++ PyDemo/快速掌握PyQt5/chap22_各种对话框介绍-3/chap22_003.py
@@ -8,13 +8,23 @@
 ################################################################################
 import sys
 from PyQt5.QtWidgets import (
-    QWidget, QApplication
+    QHBoxLayout, QPushButton, QVBoxLayout, QWidget, QApplication
 )
 
 ################################################################################
 class Demo(QWidget):
     def __init__(self) -> None:
         super().__init__()
+
+        self.save_btn = QPushButton('Save', self)
+        self.open_btn = QPushButton('Open', self)
+
+        self.v_layout = QVBoxLayout()
+        self.h_layout = QHBoxLayout()
+        self.h_layout.addWidget(self.save_btn)
+        self.h_layout.addWidget(self.open_btn)
+        self.v_layout.addLayout(self.h_layout)
+        self.setLayout(self.v_layout)
 
 ################################################################################
 if __name__ == "__main__":
