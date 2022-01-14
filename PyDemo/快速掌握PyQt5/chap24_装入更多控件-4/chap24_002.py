b8f3d5946df63786bb54bb07a1f7984e34d61a9f
iaiting <iaiting@aliyun.com>
2022-01-14 17:29:15 +0800

    (*) 优化工程

--- PyDemo/快速掌握PyQt5/chap24_装入更多控件-4/chap24_002.py
+++ PyDemo/快速掌握PyQt5/chap24_装入更多控件-4/chap24_002.py
@@ -10,7 +10,11 @@ import sys
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import (
     QApplication,
+    QComboBox,
+    QDateEdit,
+    QGridLayout,
     QLabel,
+    QLineEdit,
     QTabWidget,
     QTextEdit,
     QWidget
@@ -34,10 +38,43 @@ class Demo(QTabWidget):
 
     def tab1_init(self):
         name_label = QLabel('Name:', self.tab1)
+        name_line = QLineEdit(self.tab1)
+
+        gender_label = QLabel('Gender:', self.tab1)
+        gender_combox = QComboBox(self.tab1)
+        items = ['Please choose your gender', 'Female', 'Male']
+        gender_combox.addItems(items)
+
+        brid_label = QLabel('Birth Date:', self.tab1)
+        brid_dateedit = QDateEdit(self.tab1)
+
+        grid_layout = QGridLayout()
+        self.tab1.setLayout(grid_layout)
+        grid_layout.addWidget(name_label, 0, 0, 1, 1)
+        grid_layout.addWidget(name_line, 0, 1, 1, 1)
+        grid_layout.addWidget(gender_label, 2, 0, 1, 1)
+        grid_layout.addWidget(gender_combox, 2, 1, 1, 1)
+        grid_layout.addWidget(brid_label, 3, 0, 1, 1)
+        grid_layout.addWidget(brid_dateedit, 3, 1, 1, 1)
 
     def tab2_init(self):
         tel_label = QLabel('Tel:', self.tab2)
+        tel_line = QLineEdit()
 
+        mobile_label = QLabel('Mobile:', self.tab2)
+        mobile_line = QLineEdit()
+
+        add_label = QLabel('Address:')
+        add_line = QLineEdit()
+
+        grid_layout = QGridLayout()
+        self.tab2.setLayout(grid_layout)
+        grid_layout.addWidget(tel_label, 0, 0, 1, 1)
+        grid_layout.addWidget(tel_line, 0, 1, 1, 1)
+        grid_layout.addWidget(mobile_label, 1, 0, 1, 1)
+        grid_layout.addWidget(mobile_line, 1, 1, 1, 1)
+        grid_layout.addWidget(add_label, 2, 0, 1, 1)
+        grid_layout.addWidget(add_line, 2, 1, 1, 1)
 
 ################################################################################
 if __name__ == "__main__":
