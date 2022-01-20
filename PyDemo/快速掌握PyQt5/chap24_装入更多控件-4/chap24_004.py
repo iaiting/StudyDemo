f6f53a94bf91027c8a9cee0937135c6e4dabb5b2
iaiting <iaiting@aliyun.com>
2022-01-20 15:12:03 +0800

    (*) 优化工程

--- PyDemo/快速掌握PyQt5/chap24_装入更多控件-4/chap24_004.py
+++ PyDemo/快速掌握PyQt5/chap24_装入更多控件-4/chap24_004.py
@@ -6,15 +6,45 @@
 # 24.4 停靠窗口QDockWidget
 #
 ################################################################################
+from re import S
 import sys
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (
-    QWidget, QApplication
+    QWidget, QApplication, QMainWindow, 
+    QDockWidget, QTextEdit
 )
 
 ################################################################################
-class Demo(QWidget):
+class Demo(QMainWindow):
     def __init__(self) -> None:
         super().__init__()
+
+        self.dock1 = QDockWidget('Dock Window 1', self)
+        self.dock2 = QDockWidget('Dock Window 2', self)
+        self.dock3 = QDockWidget('Dock Window 3', self)
+
+        self.dock1.setAllowedAreas(Qt.RightDockWidgetArea | Qt.LeftDockWidgetArea)
+        self.dock2.setAllowedAreas(Qt.RightDockWidgetArea | Qt.TopDockWidgetArea)
+        self.dock3.setAllowedAreas(Qt.NoDockWidgetArea)
+
+        self.dock1.setFeatures(QDockWidget.DockWidgetMovable | QDockWidget.DockWidgetFloatable)
+        self.dock2.setFeatures(QDockWidget.DockWidgetMovable | QDockWidget.DockWidgetClosable)
+        self.dock3.setFeatures(QDockWidget.DockWidgetClosable)
+
+        self.text1 = QTextEdit()
+        self.text2 = QTextEdit()
+        self.text3 = QTextEdit()
+
+        self.dock1.setWidget(self.text1)
+        self.dock2.setWidget(self.text2)
+        self.dock3.setWidget(self.text3)
+
+        self.addDockWidget(Qt.RightDockWidgetArea, self.dock1)
+        self.addDockWidget(Qt.RightDockWidgetArea, self.dock2)
+        self.addDockWidget(Qt.RightDockWidgetArea, self.dock3)
+
+        self.center_text = QTextEdit()
+        self.setCentralWidget(self.center_text)
 
 ################################################################################
 if __name__ == "__main__":
