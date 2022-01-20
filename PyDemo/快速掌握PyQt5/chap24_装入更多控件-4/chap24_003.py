13fb2cbea28b5963859918b4c6785212ff6c4df8
iaiting <iaiting@aliyun.com>
2022-01-20 11:15:02 +0800

    (*) 优化工程

--- PyDemo/快速掌握PyQt5/chap24_装入更多控件-4/chap24_003.py
+++ PyDemo/快速掌握PyQt5/chap24_装入更多控件-4/chap24_003.py
@@ -6,11 +6,13 @@
 # 24.3 堆叠窗口QStackedWidget
 #
 ################################################################################
+from email.charset import add_alias
 from re import S
 import sys
 from PyQt5.QtWidgets import (
     QWidget, QApplication, QTextEdit, 
-    QStackedWidget, QHBoxLayout, QListWidget
+    QStackedWidget, QHBoxLayout, QListWidget, 
+    QLabel, QLineEdit, QComboBox, QGridLayout
 )
 
 ################################################################################
@@ -29,6 +31,7 @@ class Demo(QWidget):
         self.stacked_widget.addWidget(self.stack1)
         self.stacked_widget.addWidget(self.stack2)
         self.stacked_widget.addWidget(self.stack3)
+        self.stacked_widget.currentChanged.connect(lambda: print(self.stacked_widget.currentIndex()))
 
         self.list_widget = QListWidget()
         self.list_widget.addItem('Basic Info')
@@ -42,10 +45,47 @@ class Demo(QWidget):
         self.setLayout(self.h_layout)
 
     def stack1_init(self):
-        pass
+        name_label = QLabel('Name:', self.stack1)
+        name_line = QLineEdit()
+
+        gender_label = QLabel('Gender:', self.stack1)
+        gender_combox = QComboBox(self.stack1)
+
+        # bd_label = QLabel('Birth Date:', self.stack1)
+
+        g_layout = QGridLayout()
+        self.stack1.setLayout(g_layout)
+
+        g_layout.addWidget(name_label, 0, 0, 1, 1)
+        g_layout.addWidget(name_line, 0, 1, 1, 1)
+        g_layout.addWidget(gender_label, 1, 0, 1, 1)
+        g_layout.addWidget(gender_combox, 1, 1, 1, 1)
+
 
     def stack2_init(self):
-        pass
+        tel_label = QLabel('Tel:')
+        tel_line = QLineEdit()
+
+        mobile_label = QLabel('Mobile:')
+        mobile_line = QLineEdit()
+
+        add_label = QLabel('Address:')
+        add_line = QLineEdit()
+
+
+        g_layout = QGridLayout()
+        self.stack2.setLayout(g_layout)
+
+        g_layout.addWidget(tel_label, 0, 0, 1, 1)
+        g_layout.addWidget(tel_line, 0, 1, 1, 1)
+
+        g_layout.addWidget(mobile_label, 1, 0, 1, 1)
+        g_layout.addWidget(mobile_line, 1, 1, 1, 1)
+
+        g_layout.addWidget(add_label, 2, 0, 1, 1)
+        g_layout.addWidget(add_line, 2, 1, 1, 1)
+
+
 
     def change_func(self):
         print('change_func')
