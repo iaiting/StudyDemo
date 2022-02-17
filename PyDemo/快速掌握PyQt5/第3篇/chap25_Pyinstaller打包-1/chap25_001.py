cd10913f4435f373e4f8d769f68728bb45b7de3a
iaiting <iaiting@aliyun.com>
2022-02-17 17:26:46 +0800

    (*) 优化工程

--- PyDemo/快速掌握PyQt5/第3篇/chap25_Pyinstaller打包-1/chap25_001.py
+++ PyDemo/快速掌握PyQt5/第3篇/chap25_Pyinstaller打包-1/chap25_001.py
@@ -6,12 +6,17 @@
 # 25.3 打包示例
 #
 ################################################################################
+from re import S
 import sys
 from PyQt5.QtWidgets import (
     QApplication, QWidget, QLabel, QGridLayout, QLineEdit, 
-    QHBoxLayout, QVBoxLayout, QPushButton,
+    QHBoxLayout, QVBoxLayout, QPushButton, QDialog, 
+    QMessageBox
 )
 
+USER_PWD = {
+    'la_vie': 'password'
+}
 
 ################################################################################
 class Demo(QWidget):
@@ -32,7 +37,12 @@ class Demo(QWidget):
         self.h_layout = QHBoxLayout()
         self.v_layout = QVBoxLayout()
 
+        self.lineedit_init()
+        self.pushbutton_init()
+
         self.layout_init()
+
+        self.sign_page = SigninPage()
 
     def layout_init(self):
         self.grid_layout.addWidget(self.user_label, 0, 0, 1, 1)
@@ -46,6 +56,99 @@ class Demo(QWidget):
         self.v_layout.addLayout(self.grid_layout)
         self.v_layout.addLayout(self.h_layout)
         self.setLayout(self.v_layout)
+
+    def lineedit_init(self):
+        self.user_line.setPlaceholderText('Please enter your username')
+        self.pwd_line.setPlaceholderText('Please enter your password')
+        self.pwd_line.setEchoMode(QLineEdit.Password)
+
+    def pushbutton_init(self):
+        self.login_button.setEnabled(False)
+        self.login_button.clicked.connect(self.check_login_func)
+        self.signin_button.clicked.connect(self.show_signin_page_func)
+
+    def check_login_func(self):
+        print('check_login_func')
+
+    def show_signin_page_func(self):
+        print('show_signin_page_func')
+        self.sign_page.exec_()
+
+################################################################################
+class SigninPage(QDialog):
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.signin_user_label = QLabel('Username:')
+        self.signin_user_line = QLineEdit()
+
+        self.signin_pwd_label = QLabel('Password:')
+        self.signin_pwd_line = QLineEdit()
+
+        self.signin_pwd2_label = QLabel('Password:')
+        self.signin_pwd2_line = QLineEdit()
+        
+        self.signin_button = QPushButton('Sign in')
+
+        self.user_h_layout = QHBoxLayout()
+        self.pwd_h_layout = QHBoxLayout()
+        self.pwd2_h_layout = QHBoxLayout()
+        self.all_v_layout = QVBoxLayout()
+
+        self.layout_init()
+        self.lineedit_init()
+        self.pushbutton_init()
+
+    def layout_init(self):
+        self.setLayout(self.all_v_layout)
+
+        self.user_h_layout.addWidget(self.signin_user_label)
+        self.user_h_layout.addWidget(self.signin_user_line)
+
+        self.pwd_h_layout.addWidget(self.signin_pwd_label)
+        self.pwd_h_layout.addWidget(self.signin_pwd_line)
+
+        self.pwd2_h_layout.addWidget(self.signin_pwd2_label)
+        self.pwd2_h_layout.addWidget(self.signin_pwd2_line)
+
+        self.all_v_layout.addLayout(self.user_h_layout)
+        self.all_v_layout.addLayout(self.pwd_h_layout)
+        self.all_v_layout.addLayout(self.pwd2_h_layout)
+        self.all_v_layout.addWidget(self.signin_button)
+
+    def lineedit_init(self):
+        self.signin_pwd_line.setEchoMode(QLineEdit.Password)
+        self.signin_pwd2_line.setEchoMode(QLineEdit.Password)
+
+        self.signin_user_line.textChanged.connect(self.check_input_func)
+        self.signin_pwd_line.textChanged.connect(self.check_input_func)
+        self.signin_pwd2_line.textChanged.connect(self.check_input_func)
+
+    def pushbutton_init(self):
+        self.signin_button.setEnabled(False)
+        self.signin_button.clicked.connect(self.check_signin_func)
+
+    def check_input_func(self):
+        print('check_input_func')
+        if self.signin_user_line.text() and self.signin_pwd_line.text() and self.signin_pwd2_line.text():
+            self.signin_button.setEnabled(True)
+        else:
+            self.signin_button.setEnabled(False)
+
+    def check_signin_func(self):
+        print('check_signin_func')
+        if self.signin_pwd_line.text() != self.signin_pwd2_line.text():
+            QMessageBox.critical(self, 'wrong', 'Two Passwords Typed Are Not Same!')
+
+        elif self.signin_user_line.text() not in USER_PWD:
+            USER_PWD[self.signin_user_line.text()] = self.signin_pwd_line.text()
+            QMessageBox.information(self, 'information', 'Register Successfully')
+            self.signin_user_line.clear()
+            self.signin_pwd_line.clear()
+            self.signin_pwd2_line.clear()
+            self.close()
+        else:
+            QMessageBox.critical(self, 'Wrong', 'This Username Has Been Registered!')
 
 
 ################################################################################
