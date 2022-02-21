94333e7b6de1dc5990ad5ed80b058161bc1484e7
iaiting <iaiting@aliyun.com>
2022-02-21 10:04:35 +0800

    (*) 优化工程

--- PyDemo/快速掌握PyQt5/第2篇/chap24_装入更多控件-3/chap24_003.py
+++ PyDemo/快速掌握PyQt5/第2篇/chap24_装入更多控件-3/chap24_003.py
@@ -1,14 +1,36 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-
-from curses import A_ALTCHARSET
-from socket import AF_AAL5
+################################################################################
+#
+# 24.3 堆叠窗口QStackedWidget
+#
+################################################################################
+import sys
 from PyQt5.QtWidgets import (
-    QApplication
+    QApplication, QWidget, QListWidget, 
+    QHBoxLayout,
 )
-######################################################################
+
+################################################################################
+class Demo(QWidget):
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.list_widget = QListWidget()
+        self.list_widget.addItem('Basic Info')
+        self.list_widget.addItem('Contact Info')
+        self.list_widget.addItem('More Info')
+
+        self.h_layout = QHBoxLayout()
+        self.setLayout(self.h_layout)
+        self.h_layout.addWidget(self.list_widget)
+
+
+
+################################################################################
 if __name__ == "__main__":
-    print('ddd')
-
-
+    app = QApplication(sys.argv)
+    demo = Demo()
+    demo.show()
+    sys.exit(app.exec_())
