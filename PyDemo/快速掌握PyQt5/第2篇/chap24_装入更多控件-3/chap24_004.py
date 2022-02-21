26aac9ec8b8ae2dfe8a00acaf3e3cc4ce31c8f94
iaiting <iaiting@aliyun.com>
2022-02-21 14:24:30 +0800

    (*) 优化工程

--- PyDemo/快速掌握PyQt5/第2篇/chap24_装入更多控件-3/chap24_004.py
+++ PyDemo/快速掌握PyQt5/第2篇/chap24_装入更多控件-3/chap24_004.py
@@ -7,14 +7,49 @@
 #
 ################################################################################
 import sys
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (
-    QApplication, QWidget
+    QApplication, QWidget, QMainWindow, 
+    QDockWidget, QTextEdit
 )
 
 ################################################################################
-class Demo(QWidget):
+class Demo(QMainWindow):
     def __init__(self) -> None:
         super().__init__()
+
+        # 1
+        self.dock1 = QDockWidget('Dock Window 1', self)
+        self.dock2 = QDockWidget('Dock Window 2', self)
+        self.dock3 = QDockWidget('Dock WIndow 3', self)
+
+        # 2
+        self.dock1.setAllowedAreas(Qt.RightDockWidgetArea | Qt.LeftDockWidgetArea)
+        self.dock2.setAllowedAreas(Qt.RightDockWidgetArea | Qt.TopDockWidgetArea)
+        self.dock3.setAllowedAreas(Qt.NoDockWidgetArea)
+
+        # 3
+        self.dock1.setFeatures(QDockWidget.DockWidgetMovable | QDockWidget.DockWidgetFloatable)
+        self.dock2.setFeatures(QDockWidget.DockWidgetMovable | QDockWidget.DockWidgetClosable)
+        self.dock3.setFeatures(QDockWidget.DockWidgetClosable)
+
+        # 4
+        self.text1 = QTextEdit()
+        self.text2 = QTextEdit()
+        self.text3 = QTextEdit()
+
+        self.dock1.setWidget(self.text1)
+        self.dock2.setWidget(self.text2)
+        self.dock3.setWidget(self.text3)
+
+        # 5
+        self.addDockWidget(Qt.RightDockWidgetArea, self.dock1)
+        self.addDockWidget(Qt.RightDockWidgetArea, self.dock2)
+        self.addDockWidget(Qt.RightDockWidgetArea, self.dock3)
+
+        # 6
+        self.center_text = QTextEdit()
+        self.setCentralWidget(self.center_text)
 
 ################################################################################
 if __name__ == "__main__":
