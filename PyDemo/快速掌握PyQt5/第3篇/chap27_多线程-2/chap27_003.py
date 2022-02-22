0c5dbc6e6ddfe1fe5dec46bea0e9695836da3385
iaiting <iaiting@aliyun.com>
2022-02-22 09:15:46 +0800

    (*) 优化工程

--- PyDemo/快速掌握PyQt5/第3篇/chap27_多线程-2/chap27_003.py
+++ PyDemo/快速掌握PyQt5/第3篇/chap27_多线程-2/chap27_003.py
@@ -17,6 +17,23 @@ class Demo(QWidget):
     def __init__(self) -> None:
         super().__init__()
 
+        self.label = QLabel('0')
+        self.label.setAlignment(Qt.AlignCenter)
+
+        self.button = QPushButton('Start', self)
+        self.button_2 = QPushButton('Stop', self)
+
+        self.v_layout = QVBoxLayout()
+        self.setLayout(self.v_layout)
+
+        self.h_layout = QHBoxLayout()
+        self.h_layout.addWidget(self.button)
+        self.h_layout.addWidget(self.button_2)
+        
+        self.v_layout.addWidget(self.label)
+        self.v_layout.addLayout(self.h_layout)
+        
+
 ################################################################################
 if __name__ == "__main__":
     app = QApplication(sys.argv)
