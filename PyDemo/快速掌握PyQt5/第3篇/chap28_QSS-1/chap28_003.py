637a980e2e6d097a73714a12266c6da7a9456478
iaiting <iaiting@aliyun.com>
2022-02-24 09:41:42 +0800

    (*) 优化工程

--- PyDemo/快速掌握PyQt5/第3篇/chap28_QSS-1/chap28_003.py
+++ PyDemo/快速掌握PyQt5/第3篇/chap28_QSS-1/chap28_003.py
@@ -24,6 +24,21 @@ class Demo(QWidget):
         self.lineedit1 = QLineEdit()
         self.lineedit2 = SubLineEdit()
 
+        self.combox = QComboBox()
+        my_list = ['A', 'B', 'C', 'D']
+        self.combox.addItems(my_list)
+
+        self.gb = QGroupBox()
+        self.label1 = QLabel('label1')
+        self.label2 = QLabel('label2')
+        self.stack = QStackedWidget()
+        self.stack.addWidget(self.label2)
+
+        self.gb_layout = QVBoxLayout()
+        self.gb_layout.addWidget(self.label1)
+        self.gb_layout.addWidget(self.stack)
+        self.gb.setLayout(self.gb_layout)
+
         self.v_layout = QVBoxLayout()
         self.setLayout(self.v_layout)
 
@@ -31,6 +46,8 @@ class Demo(QWidget):
         self.v_layout.addWidget(self.button2)
         self.v_layout.addWidget(self.lineedit1)
         self.v_layout.addWidget(self.lineedit2)
+        self.v_layout.addWidget(self.combox)
+        self.v_layout.addWidget(self.gb)
 
 
 class SubLineEdit(QLineEdit):
