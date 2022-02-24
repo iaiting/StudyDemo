b77e214dabef19cdd7e46a1c4a3a7398018bdde1
iaiting <iaiting@aliyun.com>
2022-02-24 15:34:50 +0800

    (*) 优化工程

--- PyDemo/快速掌握PyQt5/第3篇/chap28_QSS-1/chap28_006.py
+++ PyDemo/快速掌握PyQt5/第3篇/chap28_QSS-1/chap28_006.py
@@ -8,16 +8,38 @@
 ################################################################################
 import sys
 from PyQt5.QtWidgets import (
-    QApplication, QWidget
+    QApplication, QWidget, QPushButton, QComboBox, QVBoxLayout
 )
 
 ################################################################################
 class Demo(QWidget):
-    pass
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.button1 = QPushButton('button1', self)
+        self.button2 = QPushButton('button2', self)
+        self.button2.setProperty('name', 'btn2')
+
+        my_list = ['A', 'B', 'C', 'D']
+        self.combox = QComboBox(self)
+        self.combox.addItems(my_list)
+
+        self.v_layout = QVBoxLayout()
+        self.setLayout(self.v_layout)
+
+        self.v_layout.addWidget(self.button1)
+        self.v_layout.addWidget(self.button2)
+        self.v_layout.addWidget(self.combox)
 
 ################################################################################
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     demo = Demo()
+    qss = '''
+    QPushButton:hover {background-color: red}
+    QPushButton[name='btn2']:pressed {background-color: blue}
+    QComboBox::drop-down:hover {background-color: green}
+    '''
+    demo.setStyleSheet(qss)
     demo.show()
     sys.exit(app.exec_())
