2eda0dd7235fa6684ae2ee01c55f3bfdd97c0d61
iaiting <iaiting@aliyun.com>
2022-02-25 13:53:11 +0800

    (*) 优化工程

--- PyDemo/快速掌握PyQt5/第3篇/chap30_网页交互QWebEngineView-1/chap30_001.py
+++ PyDemo/快速掌握PyQt5/第3篇/chap30_网页交互QWebEngineView-1/chap30_001.py
@@ -19,11 +19,11 @@ class Demo(QWidget):
         super().__init__()
         self.resize(1000, 600)
 
-        self.back_btn = QPushButton()
-        self.forward_btn = QPushButton()
-        self.refresh_btn = QPushButton()
-        self.zoom_in_btn = QPushButton()
-        self.zoom_out_btn = QPushButton()
+        self.back_btn = QPushButton('后退')
+        self.forward_btn = QPushButton('前进')
+        self.refresh_btn = QPushButton('刷新')
+        self.zoom_in_btn = QPushButton('缩小')
+        self.zoom_out_btn = QPushButton('放大')
         self.url_line = QLineEdit()
 
         self.browser = QWebEngineView()
@@ -32,17 +32,35 @@ class Demo(QWidget):
         self.v_layout = QVBoxLayout()
 
         self.layout_init()
+        self.btn_init()
         self.line_init()
 
     def layout_init(self):
         self.setLayout(self.v_layout)
+        self.h_layout.setSpacing(0)
+
+        self.h_layout.addWidget(self.back_btn)
+        self.h_layout.addWidget(self.forward_btn)
+        self.h_layout.addWidget(self.refresh_btn)
+
+        self.h_layout.addStretch(2)
 
         self.h_layout.addWidget(self.url_line)
+
+        self.h_layout.addStretch(2)
+
+        self.h_layout.addWidget(self.zoom_in_btn)
+        self.h_layout.addWidget(self.zoom_out_btn)
+
         self.v_layout.addLayout(self.h_layout)
         self.v_layout.addWidget(self.browser)
 
+    def btn_init(selfs):
+        pass
+
     def line_init(self):
         self.url_line.setFixedWidth(400)
+        self.url_line.setPlaceholderText('Search or enter website name')
 
 
 ################################################################################
