4e729c75c66b4ebd9381f3edcc533ee94f897a84
iaiting <iaiting@aliyun.com>
2022-04-11 14:50:57 +0800

    (*) 优化工程

--- PyDemo/快速掌握PyQt5/第3篇/chap27_多线程-3/chap27_003.py
+++ PyDemo/快速掌握PyQt5/第3篇/chap27_多线程-3/chap27_003.py
@@ -9,20 +9,32 @@
 import sys
 from PyQt5.QtWidgets import (
     QApplication, QWidget, QLabel, QPushButton, 
-    QVBoxLayout, 
+    QVBoxLayout, QHBoxLayout,
 )
 
+from PyQt5.QtCore import Qt
 ################################################################################
 class Demo(QWidget):
     def __init__(self) -> None:
         super().__init__()
 
         self.label = QLabel('0')
+        self.label.setAlignment(Qt.AlignCenter)
+
+        self.button = QPushButton('Start')
+        self.button_2 = QPushButton('Stop')
 
         self.v_layout = QVBoxLayout()
-        self.v_layout.addWidget(self.label)
-        
         self.setLayout(self.v_layout)
+
+        self.v_layout.addWidget(self.label)
+
+        self.h_layout = QHBoxLayout()
+        self.h_layout.addWidget(self.button)
+        self.h_layout.addWidget(self.button_2)
+        self.v_layout.addLayout(self.h_layout)
+        
+        
 
 
 ################################################################################
