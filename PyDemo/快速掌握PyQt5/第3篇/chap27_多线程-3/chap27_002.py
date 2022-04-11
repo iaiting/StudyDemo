a8cbb5aedf877b311d3fdf563f713b84db166c19
iaiting <iaiting@aliyun.com>
2022-04-11 11:16:26 +0800

    (*) 优化工程

---
+++ PyDemo/快速掌握PyQt5/第3篇/chap27_多线程-3/chap27_002.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+################################################################################
+#
+#
+#
+################################################################################
+import sys
+from PyQt5.QtWidgets import (
+    QApplication, QWidget
+)
+
+class Demo(QWidget):
+    pass
+
+################################################################################
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    demo = Demo()
+    demo.show()
+    sys.exit(app.exec_())
+
