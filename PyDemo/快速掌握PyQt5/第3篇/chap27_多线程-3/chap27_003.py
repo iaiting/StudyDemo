a5591b9778fb3af0a8178b028b1e15e4b3ae08ab
iaiting <iaiting@aliyun.com>
2022-04-11 18:23:18 +0800

    (*) 优化工程

--- PyDemo/快速掌握PyQt5/第3篇/chap27_多线程-3/chap27_003.py
+++ PyDemo/快速掌握PyQt5/第3篇/chap27_多线程-3/chap27_003.py
@@ -12,7 +12,11 @@ from PyQt5.QtWidgets import (
     QVBoxLayout, QHBoxLayout,
 )
 
-from PyQt5.QtCore import Qt
+################################################################################
+from PyQt5.QtCore import (
+    Qt, QThread, pyqtSignal
+)
+
 ################################################################################
 class Demo(QWidget):
     def __init__(self) -> None:
@@ -22,7 +26,13 @@ class Demo(QWidget):
         self.label.setAlignment(Qt.AlignCenter)
 
         self.button = QPushButton('Start')
+        self.button.clicked.connect(self.count_func)
+
         self.button_2 = QPushButton('Stop')
+        self.button_2.clicked.connect(self.stop_count_func)
+
+        self.my_thread = MyThread()
+        self.my_thread.my_signal.connect(self.set_label_func)
 
         self.v_layout = QVBoxLayout()
         self.setLayout(self.v_layout)
@@ -33,8 +43,39 @@ class Demo(QWidget):
         self.h_layout.addWidget(self.button)
         self.h_layout.addWidget(self.button_2)
         self.v_layout.addLayout(self.h_layout)
-        
-        
+
+
+    def count_func(self):
+        self.my_thread.is_on = True
+        self.my_thread.start()
+
+    def stop_count_func(self):
+        self.my_thread.is_on = False
+        self.my_thread.count = 0
+        self.label.setText(str(self.my_thread.count))
+
+
+    def set_label_func(self, num):
+        self.label.setText(num)
+################################################################################
+class MyThread(QThread):
+    my_signal = pyqtSignal(str)
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.count = 0
+        self.is_on = True
+
+    def run(self):
+        print('run')
+        while self.is_on:
+            print(self.count)
+            self.sleep(1)
+            self.my_signal.emit(str(self.count))
+            self.count += 1
+            
+            
+            
 
 
 ################################################################################
