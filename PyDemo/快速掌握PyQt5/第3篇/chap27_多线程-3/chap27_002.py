fee73073afc21a8d363aa5a7922ea4413190cc04
iaiting <iaiting@aliyun.com>
2022-04-11 13:21:55 +0800

    (*) 优化工程

--- PyDemo/快速掌握PyQt5/第3篇/chap27_多线程-3/chap27_002.py
+++ PyDemo/快速掌握PyQt5/第3篇/chap27_多线程-3/chap27_002.py
@@ -8,11 +8,49 @@
 ################################################################################
 import sys
 from PyQt5.QtWidgets import (
-    QApplication, QWidget
+    QApplication, QWidget, QLabel, QPushButton, QVBoxLayout
 )
+from PyQt5.QtCore import Qt, QThread, pyqtSignal
+
+class MyThread(QThread):
+    my_signal = pyqtSignal(str)
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.count = 0
+
+    def run(self):
+        while True:
+            self.sleep(1)
+            print(self.count)
+            self.my_signal.emit(str(self.count))
+
+            self.count += 1
 
 class Demo(QWidget):
-    pass
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.label = QLabel('0')
+        self.label.setAlignment(Qt.AlignCenter)
+        self.button = QPushButton('Count')
+        self.button.clicked.connect(self.count_func)
+
+        self.my_thread = MyThread()
+        self.my_thread.my_signal.connect(self.set_lable_func)
+
+        self.v_layout = QVBoxLayout(self)
+        self.setLayout(self.v_layout)
+        self.v_layout.addWidget(self.label)
+        self.v_layout.addWidget(self.button)
+
+    def count_func(self):
+        print('count_func')
+        self.my_thread.start()
+
+
+    def set_lable_func(self, num):
+        self.label.setText(num)
 
 ################################################################################
 if __name__ == "__main__":
@@ -20,4 +58,3 @@ if __name__ == "__main__":
     demo = Demo()
     demo.show()
     sys.exit(app.exec_())
-
