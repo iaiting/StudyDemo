95bd7be08d205312d971de3f6df22a312beb3f82
iaiting <iaiting@aliyun.com>
2022-04-11 10:50:35 +0800

    (*) 优化工程

--- PyDemo/快速掌握PyQt5/第3篇/chap27_多线程-3/chap27_001.py
+++ PyDemo/快速掌握PyQt5/第3篇/chap27_多线程-3/chap27_001.py
@@ -8,8 +8,9 @@
 ################################################################################
 import sys
 from PyQt5.QtWidgets import (
-    QApplication, QWidget, QPushButton
+    QApplication, QWidget, QPushButton, QLabel, QVBoxLayout
 )
+from PyQt5.QtCore import Qt
 
 
 ################################################################################
@@ -17,6 +18,22 @@ class Demo(QWidget):
     def __init__(self) -> None:
         super().__init__()
 
+        self.count = 0
+
+        self.button = QPushButton('Count')
+        self.button.clicked.connect(self.count_func)
+        
+        self.label = QLabel('0')
+        self.label.setAlignment(Qt.AlignCenter)
+
+        self.v_layout = QVBoxLayout()
+        self.setLayout(self.v_layout)
+
+        self.v_layout.addWidget(self.label)
+        self.v_layout.addWidget(self.button)
+
+    def count_func(self):
+        print('Enter count_func:')
 
 ################################################################################
 if __name__ == "__main__":
