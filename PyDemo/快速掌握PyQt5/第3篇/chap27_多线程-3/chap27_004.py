7816191c842302a111fc58fdc3d2ad6f14495c27
iaiting <iaiting@aliyun.com>
2022-04-12 11:22:27 +0800

    (*) 优化工程

--- PyDemo/快速掌握PyQt5/第3篇/chap27_多线程-3/chap27_004.py
+++ PyDemo/快速掌握PyQt5/第3篇/chap27_多线程-3/chap27_004.py
@@ -8,22 +8,51 @@
 ################################################################################
 import sys
 from PyQt5.QtWidgets import (
-    QApplication, QWidget,
+    QApplication, QWidget, QLabel, QPushButton,
+    QVBoxLayout, 
+)
+from PyQt5.QtCore import (
+    Qt, QThread, pyqtSignal
 )
 
+import urllib.request
 
 ################################################################################
 class Demo(QWidget):
     def __init__(self) -> None:
         super().__init__()
 
+        self.label = QLabel('Read to do')
+        self.button = QPushButton('Start')
+
+        self.v_layout = QVBoxLayout()
+        self.setLayout(self.v_layout)
+
+        self.v_layout.addWidget(self.label)
+
+
+################################################################################
+class CrawThread(QThread):
+    status_signal = pyqtSignal(str)
+
+    def __init__(self) -> None:
+        super().__init__()
+
+
+    def run(self):
+        self.status_signal.emit('Crawling')
+        response = urllib.request.urlopen('https://www.python.org')
+        
+        self.status_signal.emit('Saving')
+        with open('python.txt', 'w') as f:
+            f.write(response.read().decode('utf-8'))
+
+        self.status_signal.emit('Done')
+
 
 ################################################################################
 if __name__ == "__main__":
-    # app = QApplication(sys.argv)
-    # demo = Demo()
-    # demo.show()
-    # sys.exit(app.exec_())
-    import urllib.request
-    response = urllib.request.urlopen('https://www.python.org')
-    print(response.read().decode('utf-8'))
+    app = QApplication(sys.argv)
+    demo = Demo()
+    demo.show()
+    sys.exit(app.exec_())
