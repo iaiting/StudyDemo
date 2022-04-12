85c359a8db960b8bb59bf8a4e4ba24120dcac10e
iaiting <iaiting@aliyun.com>
2022-04-12 15:23:59 +0800

    (*) 优化工程

--- PyDemo/快速掌握PyQt5/第3篇/chap30_网页交互QWebEngineView-2/chap30_001.py
+++ PyDemo/快速掌握PyQt5/第3篇/chap30_网页交互QWebEngineView-2/chap30_001.py
@@ -8,14 +8,35 @@
 #
 ################################################################################
 import sys
+from PyQt5.QtCore import Qt, QUrl
+from PyQt5.QtWebEngineWidgets import QWebEngineView
 from PyQt5.QtWidgets import (
-    QApplication, QWidget
+    QApplication, QWidget, QPushButton, 
+    QHBoxLayout, QVBoxLayout
 )
 
 ################################################################################
 class Demo(QWidget):
     def __init__(self) -> None:
         super().__init__()
+        self.resize(1000, 600)
+
+        self.back_btn = QPushButton()
+        self.forward_btn = QPushButton()
+        self.refresh_btn = QPushButton()
+
+        self.h_layout = QHBoxLayout()
+        self.v_layout = QVBoxLayout()
+
+        self.layout_init()
+
+    def layout_init(self):
+        self.setLayout(self.v_layout)
+        self.v_layout.addLayout(self.h_layout)
+
+        self.h_layout.addWidget(self.back_btn)
+        self.h_layout.addWidget(self.forward_btn)
+        self.h_layout.addWidget(self.refresh_btn)
 
 
 ################################################################################
@@ -24,4 +45,3 @@ if __name__ == "__main__":
     demo = Demo()
     demo.show()
     sys.exit(app.exec_())
-
