4a2d4abfbe644cd0e7a983137548183a58eebe70
iaiting <iaiting@aliyun.com>
2022-04-12 18:36:35 +0800

    (*) 优化工程

--- PyDemo/快速掌握PyQt5/第3篇/chap30_网页交互QWebEngineView-2/chap30_001.py
+++ PyDemo/快速掌握PyQt5/第3篇/chap30_网页交互QWebEngineView-2/chap30_001.py
@@ -11,7 +11,7 @@ import sys
 from PyQt5.QtCore import Qt, QUrl
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 from PyQt5.QtWidgets import (
-    QApplication, QWidget, QPushButton, 
+    QApplication, QWidget, QPushButton, QLineEdit,
     QHBoxLayout, QVBoxLayout
 )
 
@@ -21,22 +21,48 @@ class Demo(QWidget):
         super().__init__()
         self.resize(1000, 600)
 
-        self.back_btn = QPushButton()
-        self.forward_btn = QPushButton()
-        self.refresh_btn = QPushButton()
+        self.back_btn = QPushButton('后退')
+        self.forward_btn = QPushButton('前进')
+        self.refresh_btn = QPushButton('刷新')
+
+        self.url_le = QLineEdit()
+        self.zoom_in_btn = QPushButton('放大')
+        self.zoom_out_btn = QPushButton('缩小')
+
+        self.browser = QWebEngineView()
 
         self.h_layout = QHBoxLayout()
         self.v_layout = QVBoxLayout()
 
         self.layout_init()
+        self.btn_init()
+        self.le_init()
 
     def layout_init(self):
-        self.setLayout(self.v_layout)
-        self.v_layout.addLayout(self.h_layout)
 
+        self.h_layout.setSpacing(0)
         self.h_layout.addWidget(self.back_btn)
         self.h_layout.addWidget(self.forward_btn)
         self.h_layout.addWidget(self.refresh_btn)
+
+        self.h_layout.addStretch(2)
+        self.h_layout.addWidget(self.url_le)
+        self.h_layout.addStretch(2)
+
+        self.h_layout.addWidget(self.zoom_in_btn)
+        self.h_layout.addWidget(self.zoom_out_btn)
+
+        self.setLayout(self.v_layout)
+        self.v_layout.addLayout(self.h_layout)
+        self.v_layout.addWidget(self.browser)
+
+    def btn_init(self):
+        pass
+
+    def le_init(self):
+        self.url_le.setFixedWidth(400)
+        self.url_le.setPlaceholderText('Search or enter website name')
+        pass
 
 
 ################################################################################
