d55ae6cf7a0498bd4aa5be4420be4125529b222d
iaiting <iaiting@aliyun.com>
2022-04-13 17:44:42 +0800

    (*) 优化工程

---
+++ PyDemo/快速掌握PyQt5/第4篇_技巧篇/005.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+################################################################################
+#
+# 窗口在屏幕居中显示
+#
+################################################################################
+import sys
+from PyQt5.QtWidgets import (
+    QApplication, QWidget
+)
+
+
+################################################################################
+class Demo(QWidget):
+    def __init__(self) -> None:
+        super().__init__()
+        # self.resize(100, 100)
+        # self.center()
+        self.center()
+
+    def center(self):
+        desktop = QApplication.desktop()
+        d_w = desktop.width()
+        d_h = desktop.height()
+
+        pos_x = d_w/2 - self.frameGeometry().width() / 2
+        pos_y = d_h/2 - self.frameGeometry().height() / 2
+
+        self.move(int(pos_x), int(pos_y))
+
+
+################################################################################
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    demo = Demo()
+    demo.show()
+    sys.exit(app.exec_())
