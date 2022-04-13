2c916c2dcafa64a96df002ca9fa33a05c7e0a870
iaiting <iaiting@aliyun.com>
2022-04-13 17:17:59 +0800

    (*) 优化工程

--- PyDemo/快速掌握PyQt5/第4篇_技巧篇/004.py
+++ PyDemo/快速掌握PyQt5/第4篇_技巧篇/004.py
@@ -8,14 +8,46 @@
 ################################################################################
 import sys
 from PyQt5.QtWidgets import (
-    QApplication, QWidget
+    QApplication, QWidget, QLineEdit, QPushButton,
+    QVBoxLayout, QHBoxLayout, QLabel,
 )
+from PyQt5.QtCore import QThread
 
 ################################################################################
 class Demo(QWidget):
     def __init__(self) -> None:
         super().__init__()
+        self.line = QLineEdit()
+        self.btn = QPushButton('开始爬取')
+        self.btn.clicked.connect(self.start_slot)
 
+
+        self.crawthread = CrawThread(self)
+
+        h_layout = QHBoxLayout()
+        v_layout = QVBoxLayout()
+        self.setLayout(v_layout)
+
+        h_layout.addWidget(QLabel('网址:'))
+        h_layout.addWidget(self.line)
+
+        v_layout.addLayout(h_layout)
+        v_layout.addWidget(self.btn)
+
+    def start_slot(self):
+        print('start_slot')
+        self.crawthread.start()
+
+
+################################################################################
+class CrawThread(QThread):
+    def __init__(self, demo) -> None:
+        super().__init__()
+        self.demo = demo
+
+    def run(self):
+        url = self.demo.line.text().strip()
+        print(f'要爬取的网址为: {url}')
 
 ################################################################################
 if __name__ == "__main__":
