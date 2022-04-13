3ac3e9e8ea91f7623f8195804ab41c302158c214
iaiting <iaiting@aliyun.com>
2022-04-13 13:54:12 +0800

    (*) 优化工程

--- PyDemo/快速掌握PyQt5/第4篇_技巧篇/002.py
+++ PyDemo/快速掌握PyQt5/第4篇_技巧篇/002.py
@@ -10,8 +10,10 @@ import sys
 from PyQt5.QtWidgets import (
     QApplication, QWidget, QPushButton, QLabel,
     QHBoxLayout, QVBoxLayout,
+    QLayout,
 )
 from PyQt5.QtCore import Qt
+
 
 
 ################################################################################
@@ -27,6 +29,8 @@ class Demo(QWidget):
 
         self.add_btn = QPushButton('增加文本')
         self.minus_btn = QPushButton('删除文本')
+        self.add_btn.clicked.connect(self.add_slot)
+        self.minus_btn.clicked.connect(self.minus_slot)
 
         all_v_layout = QVBoxLayout()
         self.v_layout = QVBoxLayout()
@@ -41,7 +45,21 @@ class Demo(QWidget):
         all_v_layout.addLayout(self.v_layout)
         all_v_layout.addLayout(h_layout)
         self.setLayout(all_v_layout)
+
+        all_v_layout.setSizeConstraint(QLayout.SetFixedSize)
     
+    def add_slot(self):
+        label = QLabel(str(len(self.label_list) + 1))
+        label.setAlignment(Qt.AlignCenter)
+        self.label_list.append(label)
+        self.v_layout.addWidget(label)
+
+    def minus_slot(self):
+        if len(self.label_list)  == 1:
+            return 
+        label = self.label_list.pop()
+        label.deleteLater()
+
 
 ################################################################################
 if __name__ == "__main__":
