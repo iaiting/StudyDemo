7cf443b8fc9960b7e484866183f73fdb9828f0a9
iaiting <iaiting@aliyun.com>
2022-04-13 10:47:59 +0800

    (*) 优化工程

--- PyDemo/快速掌握PyQt5/第4篇_技巧篇/001.py
+++ PyDemo/快速掌握PyQt5/第4篇_技巧篇/001.py
@@ -12,10 +12,26 @@ from PyQt5.QtWidgets import (
     QApplication, QWidget
 )
 
+
+################################################################################
 class Demo(QWidget):
     def __init__(self) -> None:
         super().__init__()
+        self.setWindowFlag(Qt.FramelessWindowHint)
 
+    def mousePressEvent(self, QMouseEvent) -> None:
+        self.start_x = QMouseEvent.x()
+        self.start_y = QMouseEvent.y()
+        print(self.start_x)
+        print(self.start_y)
+
+    def mouseMoveEvent(self, QMouseEvent) -> None:
+        print(QMouseEvent.x())
+        print(QMouseEvent.y())
+
+        dis_x = QMouseEvent.x() - self.start_x
+        dis_y = QMouseEvent.y() - self.start_y
+        self.move(self.x()+ dis_x, self.y()+ dis_y)
 
 ################################################################################
 if __name__ == "__main__":
