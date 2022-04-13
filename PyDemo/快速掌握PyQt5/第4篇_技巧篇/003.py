c305a1d56b8116d104ed12bcb06e0d70c9c12835
iaiting <iaiting@aliyun.com>
2022-04-13 15:48:02 +0800

    (*) 优化工程

--- PyDemo/快速掌握PyQt5/第4篇_技巧篇/003.py
+++ PyDemo/快速掌握PyQt5/第4篇_技巧篇/003.py
@@ -8,9 +8,11 @@
 ################################################################################
 import sys
 from PyQt5.QtWidgets import (
-    QApplication, QWidget, QPushButton, 
+    QApplication, QWidget, QPushButton, QTableWidget, QHeaderView,
+    QTableWidgetItem,
     QVBoxLayout,
 )
+from PyQt5.QtCore import Qt
 
 
 ################################################################################
@@ -19,12 +21,36 @@ class Demo(QWidget):
         super().__init__()
         self.resize(500, 300)
 
+        self.table = QTableWidget()
+        self.set_table_properties()
+
         self.btn = QPushButton('文本居中')
+        self.btn.clicked.connect(self.center_slot)
     
         v_layout = QVBoxLayout()
         self.setLayout(v_layout)
         
+        v_layout.addWidget(self.table)
         v_layout.addWidget(self.btn)
+    
+    def set_table_properties(self):
+        self.table.setColumnCount(3)
+        self.table.setRowCount(6)
+        self.table.setHorizontalHeaderLabels(['标题1', '标题2', '标题3'])
+        # self.table.horizontalHeader().setSelectionMode(QHeaderView.Stretch)
+        self.table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        self.table.verticalHeader().setHidden(True)
+
+        for row in range(self.table.rowCount()):
+            for col in range(self.table.colorCount()):
+                self.table.setItem(row, col, QTableWidgetItem(f'({row}, {col})'))
+
+
+    def center_slot(self):
+        for row in range(self.table.rowCount()):
+            for col in range(self.table.columnCount()):
+                self.table.item(row, col).setTextAlignment(Qt.AlignCenter)
+
 
 ################################################################################
 if __name__ == "__main__":
