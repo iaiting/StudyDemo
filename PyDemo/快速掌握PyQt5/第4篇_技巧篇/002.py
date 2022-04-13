fb53d516badcfc0f5ce72039da15c4457a52fac8
iaiting <iaiting@aliyun.com>
2022-04-13 11:22:09 +0800

    (*) 优化工程

--- PyDemo/快速掌握PyQt5/第4篇_技巧篇/002.py
+++ PyDemo/快速掌握PyQt5/第4篇_技巧篇/002.py
@@ -8,7 +8,7 @@
 ################################################################################
 import sys
 from PyQt5.QtWidgets import (
-    QApplication, QWidget, QPushButton, 
+    QApplication, QWidget, QPushButton, QLabel,
     QHBoxLayout, QVBoxLayout,
 )
 from PyQt5.QtCore import Qt
@@ -19,15 +19,25 @@ class Demo(QWidget):
     def __init__(self) -> None:
         super().__init__()
 
+        self.label_list = []
+        for i in range(5):
+            label = QLabel(str(i+1))
+            self.label_list.append(label)
+
         self.add_btn = QPushButton('增加文本')
         self.minus_btn = QPushButton('删除文本')
 
         all_v_layout = QVBoxLayout()
+        self.v_layout = QVBoxLayout()
         h_layout = QHBoxLayout()
 
+        for l in self.label_list:
+            self.v_layout.addWidget(l)
+            
         h_layout.addWidget(self.add_btn)
         h_layout.addWidget(self.minus_btn)
 
+        all_v_layout.addLayout(self.v_layout)
         all_v_layout.addLayout(h_layout)
         self.setLayout(all_v_layout)
     
