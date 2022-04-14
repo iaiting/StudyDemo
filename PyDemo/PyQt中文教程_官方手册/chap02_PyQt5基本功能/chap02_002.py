641df88b0fc6b0bb32a041b684c36e9140ebe23d
iaiting <iaiting@aliyun.com>
2022-04-14 17:56:40 +0800

    (*) 优化工程

--- PyDemo/PyQt中文教程_官方手册/chap02_PyQt5基本功能/chap02_002.py
+++ PyDemo/PyQt中文教程_官方手册/chap02_PyQt5基本功能/chap02_002.py
@@ -19,16 +19,13 @@ class Example(QWidget):
         self.initUI()
 
     def initUI(self):
-        self.show()
         self.setGeometry(300, 300, 300, 220)
         self.setWindowTitle('Icon')
         self.setWindowIcon(QIcon('web.png'))
-
+        self.show()
 
 ################################################################################
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     ex = Example()
-
     sys.exit(app.exec_())
-
