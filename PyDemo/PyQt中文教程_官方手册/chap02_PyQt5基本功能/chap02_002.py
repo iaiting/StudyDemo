91225fff5d55f9e60a2f2f2cbcdc681f66b93cbc
iaiting <iaiting@aliyun.com>
2022-04-14 17:29:36 +0800

    (*) 优化工程

--- PyDemo/PyQt中文教程_官方手册/chap02_PyQt5基本功能/chap02_002.py
+++ PyDemo/PyQt中文教程_官方手册/chap02_PyQt5基本功能/chap02_002.py
@@ -7,3 +7,25 @@
 #
 ################################################################################
 import sys
+from PyQt5.QtWidgets import (
+    QApplication, QWidget
+)
+from PyQt5.QtGui import QIcon
+
+################################################################################
+class Example(QWidget):
+    def __init__(self) -> None:
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.show()
+
+
+################################################################################
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    ex = Example()
+
+    sys.exit(app.exec_())
+
