51c99aa17836f57d55de8e59f764249032f39c0d
iaiting <iaiting@aliyun.com>
2022-04-20 09:51:43 +0800

    (*) 优化工程

--- PyDemo/PyQt中文教程_官方手册/chap03_菜单和工具栏/chap03_004.py
+++ PyDemo/PyQt中文教程_官方手册/chap03_菜单和工具栏/chap03_004.py
@@ -19,10 +19,30 @@ class Example(QMainWindow):
         self.initUI()
 
     def initUI(self):
+        self.statusbar = self.statusBar()
+        self.statusbar.showMessage('Ready')
+        
+        menubar = self.menuBar()
+        viewMenu = menubar.addMenu('View')
+
+        viewStatAct = QAction('view statusbar', self, checkable=True)
+        viewMenu.addAction(viewStatAct)
+
+        viewStatAct.setChecked(True)
+        viewStatAct.setStatusTip('View statusbar')
+        viewStatAct.triggered.connect(self.toggleMenu)
+
 
         self.setGeometry(300, 300, 300, 200)
         self.setWindowTitle('Check menu')
         self.show()
+        
+    def toggleMenu(self, state):
+        if state:
+            pass
+            self.statusbar.show()
+        else:
+            self.statusbar.hide()
 
 ################################################################################
 
