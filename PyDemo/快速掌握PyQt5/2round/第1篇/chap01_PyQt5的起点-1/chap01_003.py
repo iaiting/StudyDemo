cfe1629d70cbd419d1b40d4711f66d6721409120
iaiting <iaiting@aliyun.com>
2024-12-04 14:21:36 +0800

    (*) 优化工程

---
+++ PyDemo/快速掌握PyQt5/2round/第1篇/chap01_PyQt5的起点-1/chap01_003.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+################################################################################
+#
+#
+#
+################################################################################
+
+import sys
+from PyQt5.QtWidgets import QApplication, QLabel
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    label = QLabel('<font color="red">Hello</font> <h1>World</h1>')
+    label.show()
+    sys.exit(app.exec_())
