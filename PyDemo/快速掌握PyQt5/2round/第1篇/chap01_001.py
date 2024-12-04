040d3138a8d30517f27971926135e3a9cf741cdc
iaiting <iaiting@aliyun.com>
2024-12-04 13:22:09 +0800

    (*) 优化工程

---
+++ PyDemo/快速掌握PyQt5/2round/第1篇/chap01_001.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+from PyQt5.QtWidgets import QApplication, QLabel
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    label = QLabel('Hello World')
+    label.show()
+    sys.exit(app.exec_())
